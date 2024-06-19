1b7b985d0eb09c390b89d9b332ebcdd30dcc2135
nain <nain.acero24@gmail.com>
2024-06-19 18:55:02 -0500

    prueba

---
+++ ejercicio3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	double num;
+	cout << "Ingrese un número: ";
+	cin >> num;
+	
+	cout << "Valor absoluto: " << fabs(num) << endl;
+	cout << "Raíz cuadrada: " << sqrt(num) << endl;
+	cout << "Potencia (n^2): " << pow(num, 2) << endl;
+	cout << "Redondeo al entero más cercano: " << round(num) << endl;
+	cout << "Redondeo hacia arriba: " << ceil(num) << endl;
+	cout << "Redondeo hacia abajo: " << floor(num) << endl;
+	cout << "Seno: " << sin(num) << endl;
+	cout << "Coseno: " << cos(num) << endl;
+	cout << "Tangente: " << tan(num) << endl;
+	return 0;
+}
+
+
+
