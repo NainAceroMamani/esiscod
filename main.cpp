cd350b745a280fc0f9af829a30d6d5a80fd34e86
nain <nain.acero24@gmail.com>
2024-06-19 19:17:37 -0500

    prueba

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "funciones.h"
+
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int a = 10, b = 5;
+	cout << "Suma: " << suma(a, b) << endl;
+	cout << "Resta: " << resta(a, b) << endl;
+	return 0;
+}
